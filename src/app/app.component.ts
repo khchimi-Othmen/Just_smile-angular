61841d9634242028b70e51076e7288459374bd93
Othmen.Khchimi <othmen.khchimi@inetum.com>
2025-04-20 02:00:43 +0100

    feat: Add simple password protection for Dashboard route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {Router} from "@angular/router";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +8,19 @@ import {Router} from "@angular/router";
 })
 export class AppComponent {
   title = 'just_smile_ng';
+
   constructor(private router: Router) {}
 
   navigateTo(route: string): void {
-    this.router.navigate([`/${route}`]);
+    if (route === 'dashboard') {
+      const password = prompt('Enter password to access Dashboard');
+      if (password === '7amaSaida') {
+        this.router.navigate([`/${route}`]); // Navigate to dashboard if password is correct
+      } else {
+        alert('Access denied! Incorrect password.');
+      }
+    } else {
+      this.router.navigate([`/${route}`]); // Navigate normally for other routes
+    }
   }
 }
